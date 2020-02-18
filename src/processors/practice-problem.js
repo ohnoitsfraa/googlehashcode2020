18ece497c3627b33717d602ba1cd25ae35017e38
Frankie Claessens <claessensfrankie@gmail.com>
2020-02-18 21:15:04 +0100

    first try

--- src/processors/practice-problem.js
+++ src/processors/practice-problem.js
@@ -1,5 +1,25 @@
 const _ = require('lodash');
 
 module.exports = (input) => {
+    const numbers = input.map(line => line.map(x => x = +x));
+    const max = numbers[0][0];
+    const typesOfPizza = numbers[0][1];
+    const slicesPerType = numbers[1];
+    let tempSolutions = [];
+
+    slicesPerType.forEach((slicesPerPizza, index) => {
+        const otherSlicesPerType = slicesPerType.filter((slices, slicesIndex) => slicesIndex !== index);
+        let sum = slicesPerPizza;
+        tempSolutions[index] = [slicesPerPizza];
+        _.sortBy(otherSlicesPerType).reverse().forEach(otherSlice => {
+            if (sum + otherSlice <= max) {
+                sum += otherSlice;
+                tempSolutions[index].push(otherSlice);
+            }
+        });
+        tempSolutions = tempSolutions.map(tempSolution => _.sortBy(tempSolution));
+        console.log(tempSolutions);
+    });
+
     return input;
 }
